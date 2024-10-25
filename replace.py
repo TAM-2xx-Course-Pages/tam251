2b50c49528f0ed799db6c77e3707df369f07b0f3
jcrayb <jcrb7800@gmail.com>
2024-10-25 15:51:38 -0500

    all

---
+++ replace.py
@@ -0,0 +1,16 @@
+import os
+
+pages = ["pages/index.astro", "pages/info.astro", "layouts/Layout.astro"]
+
+for page in pages:
+    url = os.path.join(os.getcwd(), "src", page)
+
+    with open(url, 'r') as f:
+        data = f.read()
+
+    data = data.replace('src="/', 'src="/251-course-pages')
+
+    data = data.replace('href="/', 'href="/251-course-pages')
+
+    with open(url, 'w') as f:
+        f.write(data)
