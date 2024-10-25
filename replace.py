d5370af5ab073869f7d87fc6b4c2b7d028482163
jcrayb <jcrb7800@gmail.com>
2024-10-25 15:56:07 -0500

    actions

--- replace.py
+++ replace.py
@@ -1,6 +1,6 @@
 import os
 
-pages = ["pages/index.astro", "pages/info.astro", "layouts/Layout.astro"]
+pages = ["pages/index.astro", "pages/info.astro", "layouts/Layout.astro", "components/Navbar.astro"]
 
 for page in pages:
     url = os.path.join(os.getcwd(), "src", page)
