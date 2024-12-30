52ea6ecb892c31641160f6755cd89ab68b50fc8a
jcrayb <jcrb7800@gmail.com>
2024-12-30 23:33:17 +0100

    fixed build script again

--- replace.py
+++ replace.py
@@ -1,6 +1,6 @@
 import os
 
-pages = ["layouts/Layout.astro", "components/Navbar.astro"] + os.listdir('./pages')
+pages = ["layouts/Layout.astro", "components/Navbar.astro"] + ['./pages/' + a for a in os.listdir('./src/pages')]
 
 for page in pages:
     url = os.path.join(os.getcwd(), "src", page)
